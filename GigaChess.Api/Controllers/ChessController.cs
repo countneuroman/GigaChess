85ca41b38fd936ad2f411c1c562f0b899ca90ed0
CountNeuroman <olorinoren@gmail.com>
2023-06-17 23:47:50 +0300

    Initial commit

---
+++ GigaChess.Api/Controllers/ChessController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GigaChess.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ChessController : Controller
+{
+    [HttpPost]
+    public async Task<IActionResult> RandomMove()
+    {
+        
+        return Ok("Good");
+    }
+}
