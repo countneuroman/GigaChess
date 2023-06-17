fc060df9c63f74e2b910fedc1d1218ff4c5a667e
CountNeuroman <olorinoren@gmail.com>
2023-06-18 00:21:11 +0300

    Add frontend

---
+++ GigaChess.Web/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { Chessground } from 'chessground/chessground';
+import '@/assets/board.css';
+const boardDiv = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+    const config = {};
+    if(boardDiv.value) {
+      const ground = Chessground(boardDiv.value, config);
+    }
+  });
+</script>
+
+<template>
+  <div class="main-wrap">
+    <div class="main-board">
+      <div ref="boardDiv"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #222;
+}
+</style>
